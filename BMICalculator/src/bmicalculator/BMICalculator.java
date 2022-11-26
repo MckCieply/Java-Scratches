267df9703032787fe5c1e23e97d5f879f7b9d7e5
Alex <mwppl123@gmail.com>
2022-11-26 12:07:29 +0100

    Condition on bmi under/overweight

--- BMICalculator/src/bmicalculator/BMICalculator.java
+++ BMICalculator/src/bmicalculator/BMICalculator.java
@@ -6,14 +6,31 @@ public class BMICalculator {
     public static void main(String[] args) {
         Scanner scann = new Scanner(System.in);
         System.out.print("Input your height in meters: \n");
-        float height = scann.nextFloat();
+        float height = scann.nextFloat();   
         
         System.out.print("Input your weight: \n");
         int weight = scann.nextInt();
         
         float bmi = weight / (height * height);
+        System.out.println("Your BMI is equal to: " + bmi);
         
-        System.out.print("Your BMI is equal to: " + bmi);
+        if (bmi < 18.5){
+            //Underweight
+            System.out.println("Underweight");
+        }
+        
+        if (bmi >= 18.5 && bmi < 25){
+            //Normal
+            System.out.println("Normal");
+        }
+        if(bmi >= 25 && bmi < 30){
+            //Overweight
+            System.out.println("Overweight");
+        }
+        if(bmi >= 30){
+            //Obese
+            System.out.println("Obese");
+        }
     }
     
 }
