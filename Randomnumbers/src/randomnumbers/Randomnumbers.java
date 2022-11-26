2125bcdd82534206984c56fcd4e09d8e3d5a8b3e
Alex <mwppl123@gmail.com>
2022-11-26 12:20:54 +0100

    random % 2 == 0 then result *= random

---
+++ Randomnumbers/src/randomnumbers/Randomnumbers.java
@@ -0,0 +1,18 @@
+// Calculating multipication result on random numbers
+package randomnumbers;
+import java.util.Random;
+public class Randomnumbers {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int result = 1;
+        for(int i=0;i<10; i++){
+            int x = rand.nextInt(100);
+            if (x % 2 ==0 ){
+                result *= x;
+            }
+        }
+        System.out.println(result);
+    }
+    
+}
