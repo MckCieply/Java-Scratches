3f47fffe2ff1cf531d8ddb0d14cff58c0e4d6c12
Alex <mwppl123@gmail.com>
2022-11-27 12:52:08 +0100

    Changed for end statement for result < x

--- Randomnumbers/src/randomnumbers/Randomnumbers.java
+++ Randomnumbers/src/randomnumbers/Randomnumbers.java
@@ -6,7 +6,7 @@ public class Randomnumbers {
     public static void main(String[] args) {
         Random rand = new Random();
         int result = 1;
-        for(int i=0;i<10; i++){
+        for(int i=0;result < 10000; i++){
             int x = rand.nextInt(100);
             if (x % 2 ==0 ){
                 result *= x;
