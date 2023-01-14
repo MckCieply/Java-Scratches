a73ac9036d71cc3fcc71117713412a829d3abf07
Alex <mwppl123@gmail.com>
2023-01-14 15:52:40 +0100

    Reading from file

--- WritenReadfromFile/src/writenreadfromfile/WritenReadfromFile.java
+++ WritenReadfromFile/src/writenreadfromfile/WritenReadfromFile.java
@@ -7,15 +7,25 @@ public class WritenReadfromFile {
         Scanner scann = new Scanner(System.in);
         
         int[] input = new int[5];
-        for(int i=0; i<=input.length;i++){
+        for(int i=0; i<input.length;i++){
             System.out.print(">>>");
             int num = scann.nextInt();
             input[i] = num;
         }
         BufferedWriter writer = new BufferedWriter(new FileWriter("data.txt"));
         for(int element:input){
-            writer.write(element+"/n");
+            writer.write(element+"\n");
         }
+        writer.close();
+        BufferedReader reader = new BufferedReader(new FileReader("data.txt"));
+        String row;
+        int result = 0;
+        while((row = reader.readLine()) != null){
+            int temp = Integer.parseInt(row);
+            result += temp;
+        }
+        reader.close();
+        System.out.print("Wynik dodawania podanych liczb to: "+ result);
     }
     
 }
