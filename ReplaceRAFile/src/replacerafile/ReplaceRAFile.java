657c8713e48c8b12809ad41f6bd7e64d757720b6
Alex <mwppl123@gmail.com>
2023-01-26 12:51:16 +0100

    Finding number in text file

--- ReplaceRAFile/src/replacerafile/ReplaceRAFile.java
+++ ReplaceRAFile/src/replacerafile/ReplaceRAFile.java
@@ -1,10 +1,37 @@
 
 package replacerafile;
+import java.io.*;
 
 public class ReplaceRAFile {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        RandomAccessFile raf = new RandomAccessFile("innit.txt","rw");
+        int lookin = 0;
+        boolean flag = false;
+        while(true){
+            try{
+                String line = raf.readLine();
+                String elements[] = line.split(" +");
+                for(String element: elements){
+                    try{
+                        lookin = Integer.parseInt(element);
+                        flag = true;
+                        break;
+                    }
+                    catch(NumberFormatException e){
+                        
+                    }
+                    if (flag == true)
+                        break;
+                }
+            }
+            catch(NullPointerException e){
+                        break;
+                    }
+            }
         
+        if (lookin != 0)
+            System.out.print(lookin);
     }
     
 }
