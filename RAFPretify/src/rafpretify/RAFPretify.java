108d412842f7c90fb2744b0103a022d48b6fb05f
Alex <mwppl123@gmail.com>
2023-01-28 17:53:00 +0100

    Reading from RAF

--- RAFPretify/src/rafpretify/RAFPretify.java
+++ RAFPretify/src/rafpretify/RAFPretify.java
@@ -1,20 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package rafpretify;
 
-/**
- *
- * @author mwppl
- */
+package rafpretify;
+import java.io.*;
 public class RAFPretify {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+    public static void main(String[] args) throws IOException{
+        RandomAccessFile raf = new RandomAccessFile("oldfile.txt", "r");
+        String row;
+        while( (row = raf.readLine()) != null){
+            String[] elements = row.split(" +");
+        }
+        raf.close();
+    }   
 }
