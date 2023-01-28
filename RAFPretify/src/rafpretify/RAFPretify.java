772372214d779b5442e5e86958fac83d6259ff39
Alex <mwppl123@gmail.com>
2023-01-28 17:58:47 +0100

    writing to txt file

--- RAFPretify/src/rafpretify/RAFPretify.java
+++ RAFPretify/src/rafpretify/RAFPretify.java
@@ -5,10 +5,23 @@ public class RAFPretify {
 
     public static void main(String[] args) throws IOException{
         RandomAccessFile raf = new RandomAccessFile("oldfile.txt", "r");
+        BufferedWriter writer = new BufferedWriter(new FileWriter("newfile.txt"));
         String row;
+        int counter = 0;
         while( (row = raf.readLine()) != null){
-            String[] elements = row.split(" +");
+            String[] elements = row.trim().split(" +");
+            for(String element:elements){
+                if (counter != 4){
+                    writer.write(element.trim()+ " ");
+                    counter += 1;
+                }
+                else{
+                    writer.write("\n"+element.trim()+ " ");
+                    counter = 1;
+                }
+            } 
         }
         raf.close();
+        writer.close();
     }   
 }
