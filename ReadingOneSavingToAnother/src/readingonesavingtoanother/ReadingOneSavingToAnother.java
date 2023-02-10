71219a9157dd522316efef26af5048d805bb556d
Alex <mwppl123@gmail.com>
2023-02-10 20:18:12 +0100

    Initial commit

---
+++ ReadingOneSavingToAnother/src/readingonesavingtoanother/ReadingOneSavingToAnother.java
@@ -0,0 +1,10 @@
+// Reading from .txt file saving to raf file
+package readingonesavingtoanother;
+
+public class ReadingOneSavingToAnother {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
