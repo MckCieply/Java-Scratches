08f7abdf3f984a2ab8720fe53d147712f545534f
Alex <mwppl123@gmail.com>
2023-02-10 20:05:01 +0100

    Writing and reading first time

--- EdditingRAF/src/edditingraf/EdditingRAF.java
+++ EdditingRAF/src/edditingraf/EdditingRAF.java
@@ -1,20 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package edditingraf;
-
-/**
- *
- * @author mwppl
- */
+import java.io.*;
 public class EdditingRAF {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static double[] readRAF() throws IOException{
+        double[] results = new double[21];
+        RandomAccessFile raf = null;
+        try{
+        raf = new RandomAccessFile("plik.bin", "r");
+        int i = 0;
+        while( i < 21){
+            results[i] = raf.readDouble();
+            i+=1;
+        }
+        }
+        catch(EOFException e){
+            if(raf != null)
+                raf.close();
+        }
+        return results;
+    }
+    public static void writeRAF1() throws IOException{
+        RandomAccessFile raf = new RandomAccessFile("plik.bin", "rw");
+        for (double i=0; i <= 10; i+=0.5){
+            raf.writeDouble(i);
+        }
+        raf.close();
+    }
+    public static void change(){
+        
+    }      
+    public static void main(String[] args) throws IOException {
+        writeRAF1();
+        System.out.println("Before change: ");
+        double[] results = readRAF();
+        for(double element: results)
+            System.out.print(element+ " ");
+        System.out.println("After change: ");
     }
     
 }
