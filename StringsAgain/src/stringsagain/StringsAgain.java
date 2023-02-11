f5e751228fc237dd36b37800cafc86c11dcff709
Alex <mwppl123@gmail.com>
2023-02-11 20:58:28 +0100

    Check whether is palindom

--- StringsAgain/src/stringsagain/StringsAgain.java
+++ StringsAgain/src/stringsagain/StringsAgain.java
@@ -1,20 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package stringsagain;
 
-/**
- *
- * @author mwppl
- */
+package stringsagain;
+import java.util.Scanner;
+
 public class StringsAgain {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scann = new Scanner(System.in);
+        String input = scann.nextLine();
+        int length = input.length();
+        boolean flag = false;
+        for(int i=0; i < length/2; i++){
+            if(input.charAt(i) != input.charAt(length-1-i))
+                flag = true;
+        }
+        if (flag == true)
+            System.out.print("Slowo "+input+ " nie jest palindromem");
+        else
+            System.out.print("Slowo "+input+ " jest palindromem");
     }
     
 }
