d46af32b6f3093f415eb7b8248310b84b9476b82
Alex <mwppl123@gmail.com>
2023-02-12 19:19:59 +0100

    Basic class

--- Classes1/src/classes1/Classes1.java
+++ Classes1/src/classes1/Classes1.java
@@ -2,9 +2,19 @@
 package classes1;
 
 public class Classes1 {
-
+    int num;
+    String saidNumber;
+    public Classes1(int num, String saidNumber){
+        this.num = num;
+        this.saidNumber = saidNumber;
+    }
+    public void outNumber(){
+        System.out.print("Number "+ this.num+" is "+ this.saidNumber);
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        Classes1 num1 = new Classes1(12, "dwanascie");
+        num1.outNumber();
     }
     
 }
+
