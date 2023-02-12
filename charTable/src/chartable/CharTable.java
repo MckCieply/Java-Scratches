2df0f44b0a0f626f1c6e6e4b2e17dafe215fc6c6
Alex <mwppl123@gmail.com>
2023-02-12 23:32:49 +0100

    cant get bin to work so im using raf

--- charTable/src/chartable/CharTable.java
+++ charTable/src/chartable/CharTable.java
@@ -1,9 +1,38 @@
 
 package chartable;
-
+import java.io.*;
 public class CharTable {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        BufferedReader reader = null;
+        RandomAccessFile raf = null;
+        try{
+            reader = new BufferedReader(new FileReader("dane.txt"));
+            raf = new RandomAccessFile( "out.bin", "rw" );
+            String row;
+            while((row = reader.readLine()) != null){
+                char x = row.charAt(0);
+                raf.writeChar(x);
+            }
+        }
+        finally{
+            if (reader != null) reader.close();
+            if (raf != null) raf.close();
+            
+        }
+        RandomAccessFile raf1= null;
+        try{
+            raf1 = new RandomAccessFile("out.bin", "rw");
+            while(true){
+                System.out.println(raf1.readChar());
+            }
+        }
+        catch(EOFException e){
+            System.out.println("KONIEC PLIKU");
+            if (raf1 != null){
+                raf1.close();
+            }
+        }
         
     }
     
