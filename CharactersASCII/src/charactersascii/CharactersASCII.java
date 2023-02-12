641e3233097d56ac7181052c4fb0cc566cfde21f
Alex <mwppl123@gmail.com>
2023-02-12 22:34:42 +0100

    finding out ASCII codes

--- CharactersASCII/src/charactersascii/CharactersASCII.java
+++ CharactersASCII/src/charactersascii/CharactersASCII.java
@@ -4,7 +4,10 @@ package charactersascii;
 public class CharactersASCII {
 
     public static void main(String[] args) {
+        char x = 'A';
+        int diffrence = 97 - 65;
         
+        System.out.print((char)(x + diffrence));
     }
     
 }
