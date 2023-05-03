d1351098eb78db26ff9c9bac533c011f865bac5a
Alex <mwppl123@gmail.com>
2023-05-03 22:54:43 +0200

    Functioning code

--- Substrings/src/main/java/org/example/Main.java
+++ Substrings/src/main/java/org/example/Main.java
@@ -2,9 +2,14 @@ package org.example;
 
 public class Main {
     public static boolean solution(String str, String ending) {
+        int strLength = str.length();
+        int endingLength = ending.length();
+        String actuallEnding = str.substring(strLength - endingLength, strLength);
+        if (actuallEnding.equals(ending))
+            return true;
         return false;
     }
     public static void main(String[] args) {
-
+        System.out.print(solution("Input", "ut"));
     }
 }
