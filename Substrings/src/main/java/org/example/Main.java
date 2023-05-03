c8e1952df308b504416273bc09dd81b1b3aa8941
Alex <mwppl123@gmail.com>
2023-05-03 22:59:55 +0200

    Covering case where ending is longer than string itself

--- Substrings/src/main/java/org/example/Main.java
+++ Substrings/src/main/java/org/example/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static boolean solution(String str, String ending) {
         int strLength = str.length();
         int endingLength = ending.length();
-        String actuallEnding = str.substring(strLength - endingLength, strLength);
-        if (actuallEnding.equals(ending))
-            return true;
-        return false;
+        if (strLength >= endingLength) {
+            String actuallEnding = str.substring(strLength - endingLength, strLength);
+            if (actuallEnding.equals(ending))
+                return true;
+        }
+            return false;
     }
     public static void main(String[] args) {
         System.out.print(solution("Input", "ut"));
