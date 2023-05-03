0888d0e00c1f4d9253971e3e7ea4810cab1fe038
Alex <mwppl123@gmail.com>
2023-05-04 00:55:08 +0200

    First version with created calculating function

--- BattleGoodVsEvil/src/main/java/org/example/Main.java
+++ BattleGoodVsEvil/src/main/java/org/example/Main.java
@@ -1,20 +1,32 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static String battle(String goodAmounts, String evilAmounts) {
         String[] good = goodAmounts.split(" ");
         String[] evil = evilAmounts.split(" ");
         int[] goodStrengths = {1, 2, 3, 3, 4, 10};
         int[] evilStrengths = {1, 2, 2, 2, 3, 5, 10};
-        int goodStrength = 0;
-        int evilStrength = 0;
-        for(String element: good){
-            int strength = Integer.parseInt(element);
-
+        int goodStrength = calculateStrength(good, goodStrengths);
+        int evilStrength = calculateStrength(evil, evilStrengths);
+        if(goodStrength > evilStrength){
+            return "Battle Result: Good triumphs over Evil";
         }
-        return "";
+        else if(evilStrength > goodStrength){
+            return "Battle Result: Evil eradicates all trace of Good";
+        }
+        else{
+            return "Battle Result: No victor on this battle field";
+        }
+    }
+    public static int calculateStrength(String[] forces, int[] power){
+        int strength = 0;
+        int i = 0;
+        for(String element: forces){
+            int amount = Integer.parseInt(element);
+            strength += amount * power[i];
+            i += 1;
+        }
+        return strength;
     }
     public static void main(String[] args) {
 
