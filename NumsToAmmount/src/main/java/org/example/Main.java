68301e5f4aaba48cf6c95221889b22e5a46be277
Alex <mwppl123@gmail.com>
2023-05-04 01:10:48 +0200

    Finished

--- NumsToAmmount/src/main/java/org/example/Main.java
+++ NumsToAmmount/src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
 public class Main {
+    public static String explode(String digits) {
+        String[] digitsArray = digits.split("");
+        String output = "";
+        for(String element: digitsArray){
+            int num = Integer.parseInt(element);
+            for(int i = 0; i < num; i++){
+                output += element;
+            }
+        }
+        return output;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(explode("321"));
     }
 }
