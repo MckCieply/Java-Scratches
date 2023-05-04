0d1444e086ea81db329bd58198616118eb013ca6
Alex <mwppl123@gmail.com>
2023-05-05 00:32:46 +0200

    Innit

---
+++ RecursiveRoot/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static int digital_root(int n) {
+        // ...
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
