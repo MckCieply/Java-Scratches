c4e5444c80e69095dab54ac5dfb3798ef2812db3
Alex <mwppl123@gmail.com>
2023-05-05 01:24:26 +0200

    Innit

---
+++ WhoLikesIt/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static String whoLikesIt(String... names) {
+        //Do your magic here
+        return "";
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
