d302cf7dae92af3e2251da8c5172ab9f9d94f23e
Alex <mwppl123@gmail.com>
2023-05-05 00:56:25 +0200

    Single repetition working

--- RecursiveRoot/src/main/java/org/example/Main.java
+++ RecursiveRoot/src/main/java/org/example/Main.java
@@ -2,9 +2,29 @@ package org.example;
 
 public class Main {
     public static int digital_root(int n) {
-        // ...
+        String[] digitsString = Integer.toString(n).split("");
+        int[] digits = new int[digitsString.length];
+        int sum = 0;
+        if(digitsString.length > 1){
+            int i = 0;
+            for (String element :digitsString){
+                int digit = Integer.parseInt(element);
+                digits[i] = digit;
+                i += 1;
+            }
+            for(int element: digits){
+                sum += element;
+            }
+            if (sum > 9){
+                digital_root(sum);
+            }
+            else{
+                return sum;
+            }
+        }
+        return n;
     }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(digital_root(16));
     }
 }
