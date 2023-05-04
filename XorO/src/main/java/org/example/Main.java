a29b49e05ead29f130a2616a0c66548cedaf047f
Alex <mwppl123@gmail.com>
2023-05-05 01:12:28 +0200

    Innit

---
+++ XorO/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static boolean getXO (String str) {
+
+        // Good Luck!!
+        return true;
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
