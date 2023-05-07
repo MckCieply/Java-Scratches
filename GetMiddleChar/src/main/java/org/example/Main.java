b7ba4f745be2c776695c0ad84feaa7656a830700
Alex <mwppl123@gmail.com>
2023-05-07 20:59:37 +0200

    Finished

--- GetMiddleChar/src/main/java/org/example/Main.java
+++ GetMiddleChar/src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.List;
 public class Main {
+    public static String getMiddle(String word) {
+        List<String> chars = List.of(word.split(""));
+        if(chars.size() % 2 == 0){
+            String result = "";
+            result += chars.get(chars.size()/2 - 1) + chars.get(chars.size()/2);
+            return result;
+        }
+        else{
+            return chars.get(chars.size() / 2);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(getMiddle("middle"));
     }
 }
