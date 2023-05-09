9f6d825f5c71636d3e6e6777bee83ec18437857e
Alex <mwppl123@gmail.com>
2023-05-09 12:37:01 +0200

    Creating temp and verifying whether we've got 3 same numbers

--- GameOfGreed/src/main/java/org/example/Main.java
+++ GameOfGreed/src/main/java/org/example/Main.java
@@ -1,11 +1,38 @@
 package org.example;
 
+import java.util.LinkedList;
+
 public class Main {
     public static int greedy(int[] dice) {
-        //code here
+        // Creating LinkedList based on dice
+        LinkedList<Integer> template = new LinkedList<>();
+        for(int num: dice)
+            template.add(num);
+
+        int score = 0;
+        boolean found3 = false;
+        // Checking every number in list
+        for(int i: template){
+            // Creating empty list to store all appearances of number that is being checked
+            LinkedList<Integer> temp = new LinkedList<>();
+            // In 5 throws we can have only once 3 same digits
+            if(!found3){
+                // checking list again for all digits equal to i
+                for (int j : template) {
+                    if (j == i) {
+                        temp.add(j);
+                    }
+                }
+            }
+            // Checking if we got 3 appearances
+            if(temp.size() >= 3) {
+                found3 = true;
+            }
+
+        }
         return 0;
     }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(greedy(new int[]{1,2,5,1,1}));
     }
 }
