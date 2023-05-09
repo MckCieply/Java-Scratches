3330aeb40d6b96fa170370b7f7860e622a7370ad
Alex <mwppl123@gmail.com>
2023-05-09 10:58:06 +0200

    Innit

---
+++ RGBtoHEX/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static String rgb(int r, int g, int b) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
