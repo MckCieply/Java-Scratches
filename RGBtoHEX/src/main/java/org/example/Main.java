1262cbec8de38c6caabe873e7f82ff431f7250a6
Alex <mwppl123@gmail.com>
2023-05-09 11:28:53 +0200

    Secured for possibility of nums >255 'n' <0

--- RGBtoHEX/src/main/java/org/example/Main.java
+++ RGBtoHEX/src/main/java/org/example/Main.java
@@ -2,11 +2,15 @@ package org.example;
 
 public class Main {
     public static String rgb(int r, int g, int b) {
-        String output = "";
-        for(int element: new int[]{r,g,b}) {
-            output += String.format("%02X", element);
+        StringBuilder output = new StringBuilder();
+        for(int color: new int[]{r,g,b}) {
+            if(color < 0)
+                color = 0;
+            else if(color > 255)
+                color = 255;
+            output.append(String.format("%02X", color));
         }
-        return output;
+        return output.toString();
     }
 
     public static void main(String[] args) {
