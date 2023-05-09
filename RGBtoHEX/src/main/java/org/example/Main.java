4aae1699342cd357dd4f58ad5c2774a4418a1648
Alex <mwppl123@gmail.com>
2023-05-09 11:26:57 +0200

    Working version

--- RGBtoHEX/src/main/java/org/example/Main.java
+++ RGBtoHEX/src/main/java/org/example/Main.java
@@ -2,10 +2,14 @@ package org.example;
 
 public class Main {
     public static String rgb(int r, int g, int b) {
-        return null;
+        String output = "";
+        for(int element: new int[]{r,g,b}) {
+            output += String.format("%02X", element);
+        }
+        return output;
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(rgb(255,255,255));
     }
 }
