ca08c4244afc87a3a571b08fd46eb608cf106519
Alex <mwppl123@gmail.com>
2023-05-09 11:46:33 +0200

    Innit

---
+++ GameOfGreed/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static int greedy(int[] dice) {
+        //code here
+        return 0;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
