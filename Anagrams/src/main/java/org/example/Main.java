17917642cbab0c9aba847930785f132ee9ec6051
Alex <mwppl123@gmail.com>
2023-05-12 22:24:52 +0200

    finnished passing tests

--- Anagrams/src/main/java/org/example/Main.java
+++ Anagrams/src/main/java/org/example/Main.java
@@ -1,12 +1,21 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Main {
     public static boolean isAnagram(String test, String original) {
-
-        return false;
+            original = original.toLowerCase();
+            test = test.toLowerCase();
+            ArrayList<String> originalChars = new ArrayList<>(Arrays.asList(original.split("")));
+            ArrayList<String> testChars = new ArrayList<>(Arrays.asList(test.split("")));
+            Collections.sort(originalChars);
+            Collections.sort(testChars);
+        return originalChars.equals(testChars);
     }
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println(isAnagram("toffee", "foefets"));
     }
 }
