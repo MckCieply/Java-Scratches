0b3c2eeaba1709e52effed38c0538218f800c24a
Alex <mwppl123@gmail.com>
2023-05-15 22:18:29 +0200

    Working, not passing in CodeWars

--- ReversedArrayOfDigits/src/main/java/org/example/Main.java
+++ ReversedArrayOfDigits/src/main/java/org/example/Main.java
@@ -1,13 +1,25 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static int[] digitize(long n) {
-        // Code here
-        return new int[0];
+        ArrayList<String> nums = new ArrayList<>();
+        Collections.addAll(nums, Long.toString(n).split(""));
+        Collections.reverse(nums);
+        System.out.println(nums);
+
+        int[] output = new int[nums.size()];
+        for(String element: nums){
+            int i = nums.indexOf(element);
+            output[i] = Integer.parseInt(element);
+        }
+        return output;
     }
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println(digitize(35231));
     }
 }
