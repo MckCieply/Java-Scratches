fe14f87a2c0e27bb462e536a6915db4eb1792084
Alex <mwppl123@gmail.com>
2023-05-15 22:01:37 +0200

    Innit

---
+++ ReversedArrayOfDigits/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static int[] digitize(long n) {
+        // Code here
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
